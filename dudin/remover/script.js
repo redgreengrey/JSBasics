22041a59473e3fc424c8d1c222d4f2d03eba9f5a
redgreengrey <redbillybones@gmailcom>
2019-04-07 22:13:12 +0400

    char remover v.0.2

--- dudin/remover/script.js
+++ dudin/remover/script.js
@@ -1,22 +1,20 @@
 function charRemover() {
-    let str = document.getElementById("inputString").value;
+    let str = document.getElementById("inputString").value.toLocaleLowerCase();
     const ignore = ["?", "!", ":", ";", ",", ".", " ", "\t", "\r"];
     let letters = {}, result;
-
     let words = str.split(' ');
+
     words.forEach(function (word){
         word.split('').forEach(function (char, index) {
-            let lower = char.toLowerCase();
+            let lower = char.toLocaleLowerCase();
             if (!letters[lower] && ignore.indexOf(lower) === -1 && word.indexOf(lower, index + 1) !== -1) {
                 letters[lower] = 1;
             }
         });
-
     });
 
-    result = str.split('').filter(function(v) {
-        return !letters[v.toLowerCase()];
+    result = str.split('').filter(function (v) {
+        return !letters[v.toLocaleLowerCase()];
     }).join('');
-    // return result;
-    document.getElementById("outString").value = result;
+    document.getElementById("outString").value = result.trim();
 }
