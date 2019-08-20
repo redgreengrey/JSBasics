bd45622cc0f85c94a3d0b045911f75fd84ca3c8e
redgreengrey <redbillybones@gmailcom>
2019-08-20 17:30:11 +0400

    product list v-0.1

--- dudin/product-list/script.js
+++ dudin/product-list/script.js
@@ -1,4 +1,77 @@
-import initialList from "./initial-product-list.js";
+// import initialList from "./initial-product-list.js";
+
+let initialList = [
+    {
+        title: 'apple',
+        type: 'fruit',
+        price: 50
+    },
+    {
+        title: 'tomato',
+        type: 'vegetable',
+        price: 59
+    },
+    {
+        title: 'watermelon',
+        type: 'berries',
+        price: 4
+    },
+    {
+        title: 'avocado',
+        type: 'fruit',
+        price: 98
+    },
+    {
+        title: 'pear',
+        type: 'fruit',
+        price: 23
+    },
+    {
+        title: 'potatoes',
+        type: 'root crop',
+        price: 34
+    },
+    {
+        title: 'radish',
+        type: 'root crop',
+        price: 12
+    },
+    {
+        title: 'carrot',
+        type: 'root crop',
+        price: 29
+    },
+    {
+        title: 'cucumber',
+        type: 'vegetable',
+        price: 11
+    },
+    {
+        title: 'orange',
+        type: 'fruit',
+        price: 44
+    },
+    {
+        title: 'mandarin',
+        type: 'fruit',
+        price: 25
+    },
+    {
+        title: 'cabbage',
+        type: 'vegetable',
+        price: 19
+    },
+    {
+        title: 'pumpkin',
+        type: 'vegetable',
+        price: 23
+    },
+    {
+        title: 'strawberry',
+        type: 'berries',
+        price: 78
+    },
+];
 
 const discountButton = document.getElementById('discountButton');
 
